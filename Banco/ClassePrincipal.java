64a215115d0952d1490cfec83373f1943ae09aa6
Gabriel Ikeda <gabrielikeda55@gmail.com>
2016-10-13 16:54:43 -0300

    Classe principal do programa

---
+++ Banco/ClassePrincipal.java
@@ -0,0 +1,12 @@
+package bankpackage;
+import java.io.IOException;
+
+public class ClassePrincipal {
+    public static void main(String [] args) throws IOException
+    { 
+        RegistroConta listagem = new RegistroConta();
+        listagem.entradaConta();
+        //listagem.mostrarLista();
+        
+    }
+}
